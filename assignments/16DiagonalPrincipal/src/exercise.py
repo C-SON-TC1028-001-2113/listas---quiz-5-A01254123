a2b3588a703bf81ee63d780d8f215b7656f95aef
A01254123 <A01254123@itesm.mx>
2021-10-01 17:31:50 +0000

    Diagonal_CesarMtz

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,21 @@
-
+def valoresDiagonal(matriz):
+    diagonal=[]
+    for i in range(len(matriz)):
+        diagonal.append(matriz[i][i])
+    return diagonal    
 def main():
-  
-
+    matrices=[]
+    f=int(input(""))
+    c=int(input(""))
+    if f==c:
+        for i in range(f):
+            matrices.append([])
+            for j in range(c):
+                datos=int(input(""))
+                matrices[i].append(datos)
+        elementosDiagonal=valoresDiagonal(matrices)
+        print(elementosDiagonal)
+    else:
+        print("La matriz no es cuadrada")
 if __name__=='__main__':
     main()
